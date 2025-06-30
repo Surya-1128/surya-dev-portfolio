f2553eadb4ac2d581555f0a61cb14be7ab6185ba
Surya Arunachalam <suryaarunachalam2001@gmail.com>
2025-06-30 18:28:21 +0530

    Upated index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Portfolio from "./Portfolio";
+import Landing from "./Landing";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Portfolio />
+    <Landing />
   </React.StrictMode>
 );
